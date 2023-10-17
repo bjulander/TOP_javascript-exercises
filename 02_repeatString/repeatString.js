73e186d7b3cb8c6678f465aea2c062c597240bae
Blake Julander <blakejulander@gmail.com>
2023-10-17 07:06:23 -0700

    Completed the second exercise with asistance

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let repeatWord = ""
+    for (let i = 0; i < num; i++){
+        repeatWord += word
+    }
+    return repeatWord
 };
 
 // Do not edit below this line
