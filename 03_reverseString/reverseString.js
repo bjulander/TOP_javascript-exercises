7fd357ea5cfb8e9bd4cac2259e6566c3d4687f7a
Blake Julander <blakejulander@gmail.com>
2023-10-18 07:00:05 -0700

    Completed questions 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(newString) {
+    let reverseWord = ""
+for (let i = newString.length - 1; i >= 0; i--){
+    reverseWord += newString[i]
+}
+return reverseWord
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(someArray, singleElement) {
+    let index = someArray.indexOf(singleElement)
+    someArray.splice(index, 1)
+    return someArray
 };
 
 // Do not edit below this line
