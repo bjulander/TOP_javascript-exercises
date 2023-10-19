66aba39f832459a8ae9b6fb8b11690b8cbfb1786
Blake Julander <blakejulander@gmail.com>
2023-10-19 06:49:42 -0700

    Ordered the values of the two numbers and then put them in a loop to add all the values for a total sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0
+    let firstNum = 0
+    let secondNum = 0
+    if (num1 < num2){
+        firstNum += num1
+        secondNum += num2
+    } else {
+        firstNum += num2
+        secondNum += num1
+    }
+    for (let i = firstNum; i <= secondNum; i++ ) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
